6c5c1bd098c35bf08f22287973f871b90bb584a3
Mattia <oleandromattia@gmail.com>
2022-08-26 20:15:14 +0200

    Initial commit

---
+++ src/main/java/co/develhope/mockdatabaseexercise/MockDatabaseExerciseApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.mockdatabaseexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockDatabaseExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockDatabaseExerciseApplication.class, args);
+	}
+
+}
